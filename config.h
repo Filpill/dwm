450e6277e2199637773ef7fc281267bd04b427c0
filpill <filiplivancic@gmail.com>
2025-03-14 13:23:09 +0000

    patched in systray

--- config.h
+++ config.h
@@ -4,20 +4,25 @@
 #define TERMINAL "st"
 #define TERMCLASS "St"
 #define BROWSER "firefox"
-#define CODEEDITOR "codium"
+#define CODEEDITOR "code"
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh      = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static unsigned int borderpx  = 3;              /* border pixel of windows */
+static unsigned int snap      = 32;             /* snap pixel */
+static unsigned int gappih    = 20;             /* horiz inner gap between windows */
+static unsigned int gappiv    = 10;             /* vert inner gap between windows */
+static unsigned int gappoh    = 10;             /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 30;             /* vert outer gap between windows and screen edge */
+static int swallowfloating    = 0;              /* 1 means swallow floating windows by default */
+static int smartgaps          = 0;              /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
+static int showbar            = 1;              /* 0 means no bar */
+static int topbar             = 1;              /* 0 means bottom bar */
+static const int user_bh      = 20;             /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static char *fonts[]          = { "monospace:size=11", "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
